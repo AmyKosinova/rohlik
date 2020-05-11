d70eda04f3825921ec69f0e3ea40c69094712e46
Amy Košinová <amy.kosinova@embedit.cz>
2020-05-11 19:03:16 +0200

    Init repo

---
+++ src/test/java/com/rohlik/rohlik/RohlikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohlik.rohlik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohlikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
